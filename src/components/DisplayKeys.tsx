944921a91adcbeeab6dc4c1f5bcbd81f3a0c76e8
Justin Mascotto <mascottoj@gmail.com>
2024-03-28 07:21:49 -0400

    simplify import keys

--- src/components/DisplayKeys.tsx
+++ src/components/DisplayKeys.tsx
@@ -1,7 +1,14 @@
 import { useAppState } from "./AppState"
+import { useState, useEffect } from 'react'
+import * as bip39 from 'bip39';
 
 export const DisplayKeys = () => {
     const { state } = useAppState();
+    const [mnemonic,setMnemonic] = useState('')
+
+    useEffect(() => {
+        bip39.generateMnemonic()
+    },[state.keys])
 
     return (
         <div>
@@ -15,6 +22,9 @@ export const DisplayKeys = () => {
                     </div>    
                     <div>
                         Extended privkey: {state.keys.hdRoot.toBase58()}
+                    </div>
+                    <div>
+                        Mnemonic phrase: {mnemonic}
                     </div>
                 </div>
             )}
--- src/pages/import-keys/ImportKeys.tsx
+++ src/pages/import-keys/ImportKeys.tsx
@@ -9,29 +9,25 @@ const ImportKeys = () => {
   // State to hold the input value
   const { state, dispatch } = useAppState();
 
-  const [activeTab, setActiveTab] = useState('mnemonic');
-  const [mnemonic, setMnemonic] = useState('');
-  const [brainWallet, setBrainWallet] = useState('');
-  const [hex, setHex] = useState('');
-  const [xpriv, setXpriv] = useState('');
+  const [input, setInput] = useState('');
 
-  const importMnemonic = () => {
-    let keys = Keys.importFromMnemonic(mnemonic, networks.testnet);
+  const importMnemonic = (mnemonic:string) => {
+    const keys = Keys.importFromMnemonic(mnemonic, networks.testnet);
     dispatchImport(keys);
   }
 
-  const importBrainWallet = () => {
-    let keys = Keys.importBrainWallet(brainWallet, networks.testnet);
+  const importBrainWallet = (brainWallet:string) => {
+    const keys = Keys.importBrainWallet(brainWallet, networks.testnet);
     dispatchImport(keys);
   }
 
-  const importHex = () => {
-    let keys = Keys.importFromHex(hex, networks.testnet);
+  const importHex = (hex:string) => {
+    const keys = Keys.importFromHex(hex, networks.testnet);
     dispatchImport(keys);
   }
 
-  const importXpriv = () => {
-    let keys = Keys.importFromXpriv(xpriv, networks.testnet);
+  const importXpriv = (xpriv:string) => {
+    const keys = Keys.importFromXpriv(xpriv, networks.testnet);
     dispatchImport(keys);
   }
 
@@ -44,51 +40,18 @@ const ImportKeys = () => {
         <SidePanel></SidePanel>
         <div>
           <div>
-            <button onClick={() => setActiveTab('mnemonic')}>Import Mnemonic</button>
-            <button onClick={() => setActiveTab('hex')}>Import Hex</button>
-            <button onClick={() => setActiveTab('xpriv')}>Import Xpriv</button>
-            <button onClick={() => setActiveTab('brain')}>Import Brain Wallet</button>
+            <button onClick={() => importMnemonic(input)}>Import Mnemonic</button>
+            <button onClick={() => importHex(input)}>Import Hex</button>
+            <button onClick={() => importXpriv(input)}>Import Xpriv</button>
+            <button onClick={() => importBrainWallet(input)}>Import Brain Wallet</button>
           </div>
-          {activeTab === 'mnemonic' && (
-            <div>
-              <input
-                type="text"
-                value={mnemonic}
-                onChange={(e) => setMnemonic(e.target.value)}
-              />
-              <button onClick={importMnemonic}>Import</button>
-            </div>
-          )}
-          {activeTab === 'brain' && (
-            <div>
-              <input
-                type="text"
-                value={brainWallet}
-                onChange={(e) => setBrainWallet(e.target.value)}
-              />
-              <button onClick={importBrainWallet}>Import</button>
-            </div>
-          )}
-          {activeTab === 'hex' && (
-            <div>
-              <input
-                type="text"
-                value={hex}
-                onChange={(e) => setHex(e.target.value)}
-              />
-              <button onClick={importHex}>Import</button>
-            </div>
-          )}
-          {activeTab === 'xpriv' && (
-            <div>
-              <input
-                type="text"
-                value={xpriv}
-                onChange={(e) => setXpriv(e.target.value)}
-              />
-              <button onClick={importXpriv}>Import</button>
-            </div>
-          )}
+          <div>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </div>
       </div>
       <DisplayKeys/>
     </div>
